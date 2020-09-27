b550aea967acc04ff08dd8f8b7afe93dc4449220
Mathura <mathuradas333@gmail.com>
2020-09-28 05:14:52 +0530

    [Mathura] Add. Added equal or not method to UC-2

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -8,16 +8,33 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter x1 ");
+            Console.WriteLine("Enter x1 for first line ");
             double x1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter x2 ");
+            Console.WriteLine("Enter x2 for first line ");
             double x2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y1 ");
+            Console.WriteLine("Enter y1 for first line ");
             double y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y2 ");
+            Console.WriteLine("Enter y2 for first line ");
             double y2 = Convert.ToDouble(Console.ReadLine());
-            double lineLength = LineComparision.Program.CalLength(x1, x2, y1, y2);
-            Console.WriteLine("Line length is " + lineLength);
+
+            Console.WriteLine("Enter x1 for second line ");
+            double x12 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2 for second line ");
+            double x22 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y1 for second line ");
+            double y12 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y2 for second line ");
+            double y22 = Convert.ToDouble(Console.ReadLine());
+            double lineLength1 = LineComparision.Program.CalLength(x1, x2, y1, y2);
+            double lineLength2 = LineComparision.Program.CalLength(x12, x22, y12, y22);
+            if(lineLength1==lineLength2)
+            {
+                Console.WriteLine("Both Lines are equal in length");
+            }
+            else
+            {
+                Console.WriteLine("Both Lines are not equal in length");
+            }
         }
         static double CalLength(double x1,double x2,double y1,double y2)
         {
