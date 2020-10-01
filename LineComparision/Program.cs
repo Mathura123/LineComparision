07904f75b84c938544598ae59a60e313b21cb940
Mathura <mathuradas333@gmail.com>
2020-10-01 08:47:52 +0530

    [Mathura]Add. Unwanted folders to .gitignore

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -14,12 +14,6 @@ namespace LineComparision
                 case 1:
                     LineComparision.Program.CalculateLength();
                     break;
-                case 2:
-                    LineComparision.Program.EqualOrNot();
-                    break;
-                case 3:
-                    LineComparision.Program.CompareLines();
-                    break;
                 default:
                     Console.WriteLine("Not a valid key. Try once more");
                     LineComparision.Program.Main();
@@ -44,41 +38,7 @@ namespace LineComparision
             double len = LineComparision.Program.CallingMethod();
             Console.WriteLine("Line Length is "+ len);
         }
-        static void EqualOrNot()
-        {
-            Console.WriteLine("Enter Values for first line ");
-            double len1 = LineComparision.Program.CallingMethod();
-            Console.WriteLine("\nEnter Values for second line ");
-            double len2 = LineComparision.Program.CallingMethod();
-            if (len1 == len2)
-            {
-                Console.WriteLine("Both Lines are equal in length");
-            }
-            else
-            {
-                Console.WriteLine("Both Lines are not equal in length");
-            }
-        }
-        static void CompareLines()
-        {
-            Console.WriteLine("Enter Values for first line ");
-            double len1 = LineComparision.Program.CallingMethod();
-            Console.WriteLine("\nEnter Values for second line ");
-            double len2 = LineComparision.Program.CallingMethod();
-            if (len1>len2)
-            {
-                Console.WriteLine("First Line has more length");
-            }
-            else if(len1>len2)
-            {
-                Console.WriteLine("Second Line has more length");
-            }
-            else
-            {
-                Console.WriteLine("Both are equal");
-            }
-
-        }
+        
         static double CalLength(double x1, double x2,double y1, double y2)
         {
             double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 -y1), 2));
